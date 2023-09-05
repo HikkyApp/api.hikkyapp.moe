159cc759b91c3ed8d0f8da4cc41d927ce29b235a
Hikkywannafly <ggraygon@gmail.com>
2023-09-05 09:54:59 +0700

    feat: logic CrawlBase

--- src/core/CrawlBase.ts
+++ src/core/CrawlBase.ts
@@ -1,9 +1,54 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import { RequireAtLeastOne } from '../utils/types';
+import Monitor from './Monitor';
 
 export default class CrawlBase {
     client: AxiosInstance;
     id: string;
     name: string;
+    baseURL: string;
+    blacklist: string[];
+    monitor: Monitor;
+    locales: string[];
+    scrapingPages: number;
+
+    constructor(
+        id: string,
+        name: string,
+        axiosConfig: RequireAtLeastOne<AxiosRequestConfig, 'baseURL'>,
+    ) {
+
+        const config = {
+            headers: {
+                referer: axiosConfig.baseURL,
+                origin: axiosConfig.baseURL,
+            },
+            timeout: 20000,
+            ...axiosConfig,
+        }
+
+        this.client = axios.create(config);
+        this.baseURL = axiosConfig.baseURL;
+
+        const defaultMonitor = async () => {
+            const data = await this.client.get('/');
+
+            return data;
+        }
+
+
+        this.monitor = new Monitor(
+            defaultMonitor,
+            this.shouldMonitorChange.bind(this),
+
+        );
+
+
+    }
+
+    shouldMonitorChange(_oldPage: any, _newPage: any): boolean {
+        return false;
+    }
 
 
 
