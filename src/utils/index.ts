5cb7fe80f4f8164717dfbb0afb0fb352b0a958a3
Hikkywannafly <ggraygon@gmail.com>
2023-09-11 22:48:51 +0700

    feat: update func writeFile

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,26 @@
 import path from "path"
+import fs from 'fs';
 
 export const handlePath = (
     filePath: string,
     baseUrl: string = path.resolve(process.cwd(), './build/src')
 ) => path.join(baseUrl, filePath)
 
-export const writeFile = {
 
-}
+export const writeFile = (
+    filePath: string,
+    data: string,
+    basePath?: string,
+) => {
+    const pathname = filePath.replace(/^\.*\/|\/?[^/]+\.[a-z]+|\/$/g, ''); // Remove leading directory markers, and remove ending /file-name.extension
+
+    const pathDir = handlePath(pathname, basePath);
+
+    if (!fs.existsSync(pathDir)) {
+        fs.mkdirSync(pathDir, { recursive: true });
+    }
+
+    const fileDir = handlePath(filePath, basePath);
+
+    fs.writeFileSync(fileDir, data, { flag: 'w' });
+};
