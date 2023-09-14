a06fee7000f4100c512370d2a2eb20045a3b59ac
Hikkywannafly <ggraygon@gmail.com>
2023-09-14 10:48:41 +0700

    feat: add avs source
    Signed-off-by: Hikkywannafly <ggraygon@gmail.com>

--- src/cli/commands/crawl-init.ts
+++ src/cli/commands/crawl-init.ts
@@ -1,9 +1,18 @@
 import { Command } from 'commander';
 export default function (program: Command) {
-  return program
-    .command('crawl:init')
-    .description('Crawl Anime/Manga init')
-    .action(() => {
-      console.log('Crawl init');
-    });
+    return program
+        .command('crawl:init')
+        .description('Crawl Anime/Manga init')
+        .action(() => {
+            try {
+                console.log('Crawl init');
+
+
+
+            }
+            catch (error) {
+                console.log(error);
+                program.error(error);
+            }
+        });
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -48,3 +48,10 @@ export const isVietnamese = (text: string) => {
 
   return REGEX.test(text.toLowerCase());
 };
+
+export const fulfilledPromises = <T extends Promise<any>>(promises: T[]) =>
+  Promise.allSettled(promises).then((results) =>
+    results
+      .filter((result) => result.status === 'fulfilled')
+      .map((result) => (result as PromiseFulfilledResult<Awaited<T>>).value),
+  );
