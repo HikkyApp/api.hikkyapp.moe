d135cc451f5c567a82e7fa7c560b103b0d2c30d8
Hikkywannafly <ggraygon@gmail.com>
2023-09-23 15:43:48 +0700

    feat: add error types and handle server worker

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { Worker } from 'worker_threads';
-
+import { handlePath } from './utils';
+import { logError } from './errors/errorHandler';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+const workerMain = new Worker(handlePath('./crawl-woker.js'))
+
+workerMain.on('message', console.log);
+workerMain.on('error', logError);
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
